e28e0a8a92bb78220730f16edbda33d1fbb51f4b
ibenthinkin <benjamin.reilly@gmail.com>
2019-09-27 12:42:56 -0600

    scaffold videos banner

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,11 +4,13 @@ import './homepage.styles.scss';
 
 import TitleBanner from '../../components/title-banner/title-banner.component'
 import CourseDescription from '../../components/course-description/course-description.component'
+import VideosBanner from '../../components/videos-banner/videos-banner.component';
 
 const HomePage = () => (
   <div className='homepage'>
     <TitleBanner/>
     <CourseDescription />
+    <VideosBanner/>
   </div>
 );
 
