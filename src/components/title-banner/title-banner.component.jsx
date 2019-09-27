fd6add10832421518a086b93220b910d9e42e779
ibenthinkin <benjamin.reilly@gmail.com>
2019-09-27 15:46:21 -0600

    do more styling on landing page

--- src/components/title-banner/title-banner.component.jsx
+++ src/components/title-banner/title-banner.component.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
-
+import './title-banner.styles.scss'
+import logo from '../../assets/img/LR-white.png'
 import CustomButton from '../custom-button/custom-button.component'
 
 const TitleBanner = () =>(
     <div className='title-banner'>
-        <div className='project-title'>
-            <h2>The Basics</h2>
-            <h4>Intro To Web Development</h4>
+        <div className='content'>
+            <div className='project-title'>
+                <h1>The Basics</h1>
+                <h4>Intro To Web Development</h4>
+            </div>
+            <div className='button-container'>
+                <CustomButton text='The Basics'/>
+                <CustomButton text='Course Catalogue' />
+            </div>
         </div>
-        <div className='button-container'>
-            <CustomButton text='The Basics'/>
-            <CustomButton text='Course Catalogue' />
-        </div>
+        <img src={logo} alt='the logo for lennyroy codes'/>
     </div>
 )
 
