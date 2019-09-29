1b28e144308d6d883ca535011126325571b758fa
lennyroy <lennyroyrobles@gmail.com>
2019-09-29 16:17:39 -0400

    responsive header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,25 +6,27 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <Link className='logo-container' to="/">
-                <img className='logo' src={lcWhiteFull} alt='logo'/>
-            </Link>
-            <div className='options'>
-                <Link className='option' to='/slides'>
-                    Slides
-                </Link>
-                <Link className='option' to='/videos'>
-                    Videos
-                </Link>
-                <Link className='option' to='/challenges'>
-                    Challenges
-                </Link>
-                <Link className='option' to='/Blog'>
-                    Blog
-                </Link>
+            <div className='header'>
+                <div className="header-wrapper">
+                    <Link className='logo-container' to="/">
+                        <img className='logo' src={lcWhiteFull} alt='logo'/>
+                    </Link>
+                    <div className='options'>
+                        <Link className='option' to='/slides'>
+                            Slides
+                        </Link>
+                        <Link className='option' to='/videos'>
+                            Videos
+                        </Link>
+                        <Link className='option' to='/challenges'>
+                            Challenges
+                        </Link>
+                        <Link className='option' to='/Blog'>
+                            Blog
+                        </Link>
+                    </div>
+                </div>
             </div>
-        </div>
     )
 }
 
