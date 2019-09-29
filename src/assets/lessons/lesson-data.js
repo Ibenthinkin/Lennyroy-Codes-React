c4a30f121b73da122b2f1009a0d1ebffd6f52d50
ibenthinkin <benjamin.reilly@gmail.com>
2019-09-29 16:03:12 -0600

    start to make object of lesson info

---
+++ src/assets/lessons/lesson-data.js
@@ -0,0 +1,48 @@
+
+const LESSON_DATA = [
+    {
+        id: 1,
+        moduleTitle: 'The Basics',
+        routeName: 'thebasics',
+        lessons: [
+            {
+                id: 1,
+                title: 'Course Introduction',
+                routeName: 'courseintroduction',
+                template: '1'
+
+            }
+        ]
+    },
+    {
+        id: 2,
+        moduleTitle: 'Intro To HTML',
+        routeName: 'introtohtml',
+        lessons: [
+            {
+                id: 1,
+                template: '0',
+                title: 'First Steps in HTML',
+                routeName: 'firststepsinhtml',
+                subtitle: 'Learn the layout and features of this course. We will be diving into how HTML works conceptually and even seeing a few basics tags in action!',
+                bannerImageUrl: 'https://raw.githubusercontent.com/lennyroyroy/basics-image/master/Basics%20Screenshots/Screen%20Shot%202019-09-02%20at%206.35.34%20PM.png',
+                slidesUrl: 'https://slides.com/lennyroyroy/deck#/3',
+                slidesDownloadURL: 'https://www.docdroid.net/QudGjTa/the-basics-intro-to-web-development-lesson-1-intro-to-html.pdf',
+                codePenDescription: 'What did you have for breakfast today?',
+                codePenUrl: 'https://codepen.io/lennyroycodes/pen/PvVzJz',
+                videoDescription: 'Learn the layout and features of this course. We will be diving into how HTML works conceptually and even seeing a few basics tags in action!',
+                youtubeUrl: 'https://youtu.be/9jwfMeNF68k',
+                helpfulLinks: {}
+            }
+        ]
+    },
+
+
+
+
+
+
+
+]
+
+export default LESSON_DATA;
