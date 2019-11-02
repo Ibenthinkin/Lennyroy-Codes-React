000dae8b32e6b3e6c26aeab37da7cfab5a25619a
lennyroy <lennyroyrobles@gmail.com>
2019-11-02 12:05:32 -0400

    begin footer styles

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -5,15 +5,56 @@ import { Link } from 'react-router-dom';
 import './footer.styles.scss';
 
 const Footer = () => (
-    <div className="footer-wrapper">
+    <section className="footer-wrapper">
+        <div className="footer-flex">
+            <div className="footer-logo">
+                <p>logo</p>
+            </div>
+            <div className="footer-container">
+                <div className="footer-navigation">
+                    <h5>Heading</h5>
+                    <ul>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                    </ul>
+                </div>
+                <div className="footer-sections">
+                    <h5>Heading</h5>
+                    <ul>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                    </ul>
+                </div>
+                <div className="footer-links">
+                    <h5>Heading</h5>
+                    <ul>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                        <li>Item</li>
+                    </ul>
+                </div>
+        </div>
+
+        </div>
 
 
 
-    <div className="footer-bottom">
-        <small>Copyright 2019 © Lennyroy Codes</small>
+        <div className="footer-bottom">
+            <small>Copyright 2019 © Lennyroy Codes</small>
 
-    </div>
-    </div>
+        </div>
+    </section>
 )
 
 export default Footer
